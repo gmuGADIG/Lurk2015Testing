c21330fecbe8f788f778a709c227e869b7ec56a0
Oscar Quinteros <oquinter@gmu.edu>
2015-10-15 21:05:52 -0400

    Enemy follows player
    Need to make enemy move. Can detect and name platforms

--- LurkTestBuild/Assets/Scripts/NPCs/Enemy.cs
+++ LurkTestBuild/Assets/Scripts/NPCs/Enemy.cs
@@ -5,5 +5,28 @@ public class Enemy : MonoBehaviour {
 	
 	//the current aggro
 	public GameObject aggro;
+    public Vector3 hitbox;
+    public float range;
+    void Update()
+    {
+       RaycastHit2D line = Physics2D.Linecast(this.transform.position + hitbox, aggro.transform.position);
+       Debug.Log("I see: " + line.collider.name);
+       float adjRange = range;
+       while (line.collider.name.Equals(aggro.name) == false && line.distance <= adjRange)
+       {
+           adjRange -= line.distance;
+           Debug.Log("Range: " + adjRange);
+           line = Physics2D.Linecast(line.transform.position + hitbox, aggro.transform.position);
+           Debug.Log("I see: " + line.collider.name);
+           
+       }
+       if (line.collider.name.Equals(aggro.name) && line.distance <= range)
+       {
+            //Debug.Log("found at: " + line.transform.position);
+
+       }
+        //else
+            //Debug.Log("nada");
+    }
 	
 }
