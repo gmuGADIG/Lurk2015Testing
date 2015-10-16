9a4d41fbbc6bc38d045e3b6ab9ea8a26ef82f97d
Jacob Flesher <flesherjacob@gmail.com>
2015-10-15 20:42:40 -0400

    messy boids implementation: fix on own time

--- LurkTestBuild/Assets/Scripts/NPCs/Swarmer.cs
+++ LurkTestBuild/Assets/Scripts/NPCs/Swarmer.cs
@@ -7,7 +7,10 @@ using System.Collections.Generic;
 public class Swarmer : MonoBehaviour {
 	
 	static List<Swarmer> swarm;
-	
+
+    public float speed = 2;
+    public Weights weights;
+
 	//components;
 	Enemy en;
 	Rigidbody2D rb;
@@ -23,9 +26,40 @@ public class Swarmer : MonoBehaviour {
 		swarm.Add(this);
 	}
 	
+    void FixedUpdate() {
+        Vector2 sum = Vector2.zero;
+        float count = 0;
+        sum = rb.velocity * weights.velocity;
+        count += weights.velocity;
+        Vector2 away = Vector2.zero; 
+        foreach (Swarmer s in swarm) {
+            if (s != this) {
+                sum += s.rb.velocity * weights.follow;
+                count += weights.follow;
+                away += (Vector2) (transform.position - s.transform.position);
+            }
+        }
+        away /= swarm.Count - 1;
+        sum += away * weights.avoid;
+        count += weights.avoid;
+        if (en.aggro != null) {
+            sum += (Vector2) (en.aggro.transform.position - transform.position) * weights.aggro;
+            count += weights.aggro;
+        }
+        sum /= count;
+        rb.velocity = sum.normalized * speed;
+    }
+
 	void OnDisable() {
 		swarm.Remove(this);
 	}
 	
+    [System.Serializable]
+    public class Weights {
+        public float velocity = 1;
+        public float aggro = 1;
+        public float follow = 1;
+        public float avoid = 1;
+    }
 	
 }
