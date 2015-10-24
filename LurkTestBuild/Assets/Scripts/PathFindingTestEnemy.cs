f273021901a80802e8905531554032cf6d986882
Robert Ota Dieterich <nobunagaota@gmail.com>
2015-10-24 10:35:29 -0400

    Playing around a bit with RigidBody2D as controller for enemy. Not promising.

--- LurkTestBuild/Assets/Scripts/PathFindingTestEnemy.cs
+++ LurkTestBuild/Assets/Scripts/PathFindingTestEnemy.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class PathFindingTestEnemy : MonoBehaviour {
 
+    private float JumpTimer = 0.0f;
+    private float JumpForce = 1200.0f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,7 +13,16 @@ public class PathFindingTestEnemy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        int foo = 4;
-        ++foo;
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(1.0f, 0.0f);
+
+        JumpTimer += Time.deltaTime;
+        if(JumpTimer > 2.0f)
+        {
+            JumpTimer = 0.0f;
+
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * JumpForce);
+        }
+
 	}
 }
