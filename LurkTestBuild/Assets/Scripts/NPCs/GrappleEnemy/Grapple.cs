a5de176f51de2bc18afbec1f7ec1b3d224b88192
Veloxiac <matthewkelly95@gmail.com>
2015-10-23 23:46:36 -0400

    added cooldown to GrappleEnemy

--- LurkTestBuild/Assets/Scripts/NPCs/GrappleEnemy/Grapple.cs
+++ LurkTestBuild/Assets/Scripts/NPCs/GrappleEnemy/Grapple.cs
@@ -5,20 +5,24 @@ public class Grapple : MonoBehaviour {
 
     public Transform sightStart, sightEnd;
     public GameObject projectile;
+    public float coolDown = 1.00f;
     public float pullSpeed;
 
+    private float timeLeft = 0f;
     private GameObject player = null;
 
 	// Update is called once per frame
 	void Update () {
+        timeLeft -= Time.deltaTime;
         if (player == null) {
             Debug.DrawLine(sightStart.position, sightEnd.position, Color.red);
             LayerMask playerLayer = (1 << 0); 
             if (Physics2D.Linecast(sightStart.position, sightEnd.position, playerLayer) &&
                 Physics2D.Linecast(sightStart.position, sightEnd.position, playerLayer).collider.gameObject.tag == "Player") {
-                if (!projectile.activeSelf) {
+                if (!projectile.activeSelf && timeLeft<= 0f) {
                     projectile.transform.position = sightStart.position;
                     projectile.SetActive(true);
+                    timeLeft = coolDown;
                 }
             }
         } else {
