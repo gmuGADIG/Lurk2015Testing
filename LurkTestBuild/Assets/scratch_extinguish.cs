65e440e1fb458f925a7037e504579737b46b4826
Vikingur <voskarss@gmu.edu>
2015-11-05 17:20:38 -0500

    Improved Flammable and Extinguish
    Now based on triggers instead of collisions. Flammable objects are lit
    by trigger events, and snuffed out by trigger events, depending on the
    script of the object being collided with.

--- LurkTestBuild/Assets/scratch_extinguish.cs
+++ LurkTestBuild/Assets/scratch_extinguish.cs
@@ -5,11 +5,19 @@ public class scratch_extinguish : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		scratch_flammable lighter = col.gameObject.GetComponent<scratch_flammable>();
+		if(lighter && lighter.lit == true) {
+			lighter.lit = false;
+			return;
+		}
 	}
 }
--- LurkTestBuild/Assets/scratch_flammable.cs
+++ LurkTestBuild/Assets/scratch_flammable.cs
@@ -5,34 +5,55 @@ public class scratch_flammable : MonoBehaviour {
 
 	//Will the object be spawned lit or not?
 	public bool lit = false;
+	Collider2D[] sources;
+	Collider2D hotspot;
+	Light fireLight;
 
 	void Start () {
-		
+		fireLight = (Light)this.gameObject.GetComponent<Transform>().Find ("FlameLight").GetComponent("Light");
+		hotspot = this.GetComponent<Collider2D>();
 	}
 	
 	void Update () {
+		fireLight.enabled = this.lit;
+		//Also make calls here to whatever graphical or instantiation is necessary (Animations, lights, etc).
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		scratch_extinguish snuff = col.gameObject.GetComponentInChildren<scratch_extinguish> ();
+		if(snuff && this.lit == true){
+			this.lit = false;
+			fireLight.enabled = false;
+			//This is so extinguishing takes precedence over being lit.
+			return;
+		}
+
+		Debug.Log (col.gameObject);
+		Debug.Log (col.gameObject.GetComponent<scratch_flammable>());
+		scratch_flammable lighter = col.gameObject.GetComponent<scratch_flammable>();
 		
+		if(lighter && lighter.lit == true && this.lit != true) {
+			this.lit = true;
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D col){
-
-		//Are the two hotspots touching?
-		//Is the other object lit?
-		scratch_flammable lighter = col.gameObject.GetComponent<scratch_flammable>();
-		if(lighter && lighter.lit == true) {
-			this.lit = true;
-			//Also make calls here to whatever graphical or instantiation is necessary (Animations, lights, etc).
-			return;
-		}
-		//Else, if it collides with an extinguisher, it is no longer lit
-		scratch_extinguish snuff = col.gameObject.GetComponent<scratch_extinguish>();
-		if (snuff) {
-			Debug.Log ("Yo!");
-		}
+		scratch_extinguish snuff = col.gameObject.GetComponentInChildren<scratch_extinguish> ();
 		if(snuff && this.lit == true){
-			Debug.Log ("Also Yo!");
 			this.lit = false;
-			//Same as above, make graphical adjustments here.
+			fireLight.enabled = false;
+			//This is so extinguishing takes precedence over being lit.
+			return;
+			Debug.Log ("This shit don't work right!");
+		}
+
+		scratch_flammable lighter = col.gameObject.GetComponent<scratch_flammable>();
+
+		if(lighter && lighter.lit == true && this.lit != true) {
+			this.lit = true;
+			Debug.Log ("I changed light status! "+this.gameObject.ToString ());
 		}
 	}
+
+
 }
