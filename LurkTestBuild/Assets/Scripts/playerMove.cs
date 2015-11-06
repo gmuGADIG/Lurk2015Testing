5b046bf936e9229bb62848ee8490099cfca211e6
GuitarBro <andrew.arv@gmail.com>
2015-11-05 20:50:21 -0500

    Ladders
    Still a bit buggy

--- LurkTestBuild/Assets/Scripts/playerMove.cs
+++ LurkTestBuild/Assets/Scripts/playerMove.cs
@@ -8,6 +8,7 @@ public class playerMove : MonoBehaviour {
 	public float accel = 10;
 	public float decel = 1.2f;
 	public float jumpStrength = 20;
+	public float ladderClimbSpeed = 2f;
 
 	public int coins = 0;
 
@@ -18,6 +19,8 @@ public class playerMove : MonoBehaviour {
 
 	private Rigidbody2D rb;
 	private GameObject cam;
+	private bool onLadder = false;
+	private int fallClamp = -9999;
 
 	// Use this for initialization
 	void Start () {
@@ -28,19 +31,47 @@ public class playerMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float horizontal = Input.GetAxis ("Horizontal");
-		//rb.AddForce (new Vector2 (Mathf.Clamp(horizontal * accel, -maxSpeed, maxSpeed), 0));
-        //rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed) / decel, rb.velocity.y);
-        rb.velocity = new Vector2(Mathf.Clamp(horizontal * accel, -maxSpeed, maxSpeed), rb.velocity.y);
+		if (onLadder) {
+			horizontal = 0;
+		}
+		rb.velocity = new Vector2(Mathf.Clamp(horizontal * accel, -maxSpeed, maxSpeed), Mathf.Clamp(rb.velocity.y, fallClamp, 9999));
         Collider2D[] colResults = new Collider2D[1];
 		grounded = Physics2D.OverlapAreaNonAlloc(top_left.position, bottom_right.position, colResults, ground_layers);
-		if(Input.GetAxis("Vertical") > 0.01 && grounded > 0){
+		if(Input.GetAxis("Jump") > 0.01 && grounded > 0){
 			rb.velocity = new Vector2(rb.velocity.x, jumpStrength);
 		}
 	}
 
-	/*void LateUpdate(){
-		Vector3 targetPos = transform.position; //+ new Vector3(rb.velocity.x, rb.velocity.y, 0);
-		targetPos.z = -10;
-		cam.transform.position = Vector3.Lerp (cam.transform.position, targetPos, Time.deltaTime*8);
-	}*/
+
+	void OnTriggerStay2D(Collider2D col){
+		if (!onLadder && col.transform.tag == "Ladder" && Mathf.Abs(Input.GetAxis("Vertical")) > 0.01) {
+			// Get on ladder
+			Vector3 ladderPos = col.transform.position;
+			ladderPos.y = transform.position.y;
+			transform.position = ladderPos;
+			onLadder = true;
+			rb.isKinematic = true;
+			fallClamp = 0;
+		}
+		if (onLadder && col.transform.tag == "Ladder"){
+			float climb = 0;
+			if(Input.GetAxis("Vertical") > 0.01){
+				// go up
+				climb = ladderClimbSpeed;
+			}else if (Input.GetAxis("Vertical") < -0.01){
+				//go down
+				climb = -ladderClimbSpeed;
+				fallClamp = -9999;
+			}
+			rb.velocity = new Vector2(0, climb);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		if (onLadder && col.transform.tag == "Ladder") {
+			onLadder = false;
+			rb.isKinematic = false;
+			fallClamp = -9999;
+		}
+	}
 }
