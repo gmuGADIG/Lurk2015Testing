5b075ba79fb9ef1f14623d122d15b047b2c5c88f
Tanner Grehawick <ggrehawi@gmu.edu>
2015-11-05 20:50:54 -0500

    archer window

---
+++ LurkTestBuild/Assets/Scripts/NPCs/LinearProjectile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class LinearProjectile : MonoBehaviour {
+
+	public Vector2 velocity;
+	public float despawnTimer = 5;
+
+	void Start() {
+		Destroy(gameObject, despawnTimer);
+	}
+
+	void Update() {
+		transform.position += (Vector3) velocity * Time.deltaTime;
+		transform.right = velocity.normalized;
+	}
+
+
+
+}
