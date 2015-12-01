85147c87663b4bcf97185a37778ba9642bd60220
Vikingur <voskarss@gmu.edu>
2015-12-01 18:31:02 -0500

    Gas Pocket detection working.
    For the moment, there is only a debug log to indicate when the player is
    killed. What are the standards we are using for damage and explosive
    force?

--- LurkTestBuild/Assets/scratch_gas_pocket.cs
+++ LurkTestBuild/Assets/scratch_gas_pocket.cs
@@ -4,20 +4,31 @@ using System.Collections;
 public class scratch_gas_pocket : MonoBehaviour {
 
 	public float bomb_radius = 3;
-	
+	public float bomb_recharge = 5;
+	private float last_detonation_time;
+	public bool active = true;
+	//If one_blast is true, the object will destroy itself after detonating the first time.
+	public bool one_blast = false;
+
 	// Use this for initialization
 	void Start () {
-		
+		last_detonation_time = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (active == false) {
+			if (Time.time - last_detonation_time >= bomb_recharge) {
+				active = true;
+			}
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
-		if (other.gameObject.GetComponent<scratch_flammable> ().lit) {
+		if (active && other.gameObject.GetComponent<scratch_flammable> ().lit) {
 			this.ignite();
+			active = false;
+			last_detonation_time = Time.time;
 		}
 	}
 
@@ -28,12 +39,13 @@ public class scratch_gas_pocket : MonoBehaviour {
 		Vector3 thispos = this.gameObject.transform.position;
 		Collider2D[] affected = Physics2D.OverlapCircleAll (new Vector2(thispos.x, thispos.y), bomb_radius);
 		foreach (Collider2D target in affected) {
-			if(target.gameObject.GetComponent<playerMove>()){
-				Destroy (target.gameObject);
+			if(target.gameObject.tag == "Player"){
+				Debug.Log ("Player killed!");
 			}
 		}
-		//Then destroy this object.
-		Destroy(this);
+		if (one_blast) {
+			DestroyObject (this.gameObject);
+		}
 	}
 }
 
