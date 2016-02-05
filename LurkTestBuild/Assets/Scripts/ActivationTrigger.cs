468ddd34c8e0f671b8ef29729e6d7d52952833c8
Tanner Grehawick <ggrehawi@gmu.edu>
2016-02-04 20:02:00 -0500

    the birds on a curve

---
+++ LurkTestBuild/Assets/Scripts/ActivationTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ActivationTrigger : MonoBehaviour {
+
+	public UnityEvent onTriggerEnter;
+	public UnityEvent onTriggerStay;
+	public UnityEvent onTriggerExit;
+
+	void OnTriggerEnter() {
+		onTriggerEnter.Invoke();
+	}
+
+	void OnTriggerStay() {
+		onTriggerStay.Invoke();
+	}
+
+	void OnTriggerExit() {
+		onTriggerExit.Invoke();
+	}
+
+}
