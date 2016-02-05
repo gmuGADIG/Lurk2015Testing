04a65878694b5a5aa8f7a2dd2b52f7c9e32993c6
Cethin <cethin@live.com>
2016-02-05 12:24:57 -0500

    Raycasting for throw prediction
    Raycast against default and ground layers to check for collision and
    draws prediction to that point and stops.

--- LurkTestBuild/Assets/Scripts/Throw.cs
+++ LurkTestBuild/Assets/Scripts/Throw.cs
@@ -5,23 +5,25 @@ using System.Collections.Generic;
 // http://forum.unity3d.com/threads/projectile-prediction-line.143636/
 public class Throw : MonoBehaviour
 {
-	#region Parameters
+    #region Parameters
+    public LayerMask mask;                  // Layer mask for throw prediction - (DO NOT CHANGE IF YOU DO NOT KNOW WHAT IT DOES)
 	public Vector2 standardForce;			// Force to be added to obj after rotated by angle
 	public float forceMult = 10;
-	public float timeSeg = .05f;			// time per segment (in seconds)
-	public int maxVerts = 51;				// Maximum number of vertices to use for line
+	public float timeSeg = .05f;			// time per segment (in seconds) (resolution)
+	public int maxVerts = 51;				// Maximum number of vertices to use for line (length)
 	public float rotSpeed = 20f;            // Degrees to rotate/sec
     float angle = 0f;                       // Direction of throw
 
     #region LineProperties
-    Color startColor = Color.red;
-	Color endColor = Color.magenta;
-	float startAlpha = .5f;
-	float endAlpha = .5f;
+    public Color startColor = Color.yellow;
+	public Color endColor = Color.yellow;
+	float startAlpha = .75f;
+	float endAlpha = .75f;
 	float startWidth = .1f;
 	float endWidth = .1f;
 	public Material mat;
-	float z = -1f;					// Z height to draw line
+	float z = -1f;					        // Z height to draw line
+    List<Vector3> points = new List<Vector3>();
 	#endregion
 
 	LineRenderer line;
@@ -38,21 +40,24 @@ public class Throw : MonoBehaviour
 		standardForce = transform.right * forceMult;
 		force = transform.right * forceMult;
         player = GetComponent<playerMove>();
+        inv = GetComponent<Inventory>();
 
-		// Setup line
-		line = gameObject.AddComponent<LineRenderer>();
-		line.transform.parent = transform.parent;
-		startColor.a = startAlpha;
-		endColor.a = endAlpha;
-		line.SetColors(startColor, endColor);
-		line.SetWidth(startWidth, endWidth);
-		line.material = mat;
-		inv = GetComponent<Inventory> ();
 	}
+
+    private void setupLine()
+    {
+        line = gameObject.AddComponent<LineRenderer>();
+        line.transform.parent = transform.parent;
+        startColor.a = startAlpha;
+        endColor.a = endAlpha;
+        line.SetColors(startColor, endColor);
+        line.SetWidth(startWidth, endWidth);
+        line.material = mat;
+    }
 	#endregion
 
 	#region Update
-	void Update()
+	void FixedUpdate()
 	{
 		if(Input.GetAxisRaw ("C") == 1 && inv.GetItem() != null)
 		{
@@ -97,25 +102,49 @@ public class Throw : MonoBehaviour
 	#region UpdateTrajectory
 	void UpdateTrajectory()
 	{
-		if(line == null)
-			line = gameObject.AddComponent<LineRenderer>();
-		line.SetVertexCount(maxVerts);
-		
-		Vector2 position = transform.position;
-		Vector2 velocity = force;
+        Vector2 position = transform.position;
+        Vector2 nextPosition = position;
+
+        Vector2 velocity = force;
         if(player.getDirection())
         {
             velocity.x = Mathf.Abs(velocity.x);
         }else
             velocity.x = -Mathf.Abs(velocity.x);
-        for (int i = 0; i < maxVerts; ++i)
-		{
-			line.SetPosition(i, new Vector3(position.x, position.y, z));
-			
-			position += velocity * timeSeg + 0.5f * Physics2D.gravity * timeSeg * timeSeg;
-			velocity += Physics2D.gravity * timeSeg;
-		}
-	}
+
+        int i = 0;
+        do
+        {
+            //Debug.Log(i + " " + velocity);
+            points.Add(new Vector3(position.x, position.y, z));
+            nextPosition += velocity * timeSeg + 0.5f * Physics2D.gravity * timeSeg * timeSeg;
+            // Raycast for collision
+            RaycastHit2D hit = Physics2D.Linecast(position, nextPosition, mask);
+            if (hit)
+            {
+                Vector2 point = hit.point;
+                points.Add(new Vector3(point.x, point.y, z));
+                drawLine();
+                return;
+            }
+            position = nextPosition;
+
+            velocity += Physics2D.gravity * timeSeg;
+        } while (++i < maxVerts/* && !collision*/);
+
+        drawLine();
+    }
+
+    private void drawLine()
+    {
+
+        if (line == null)
+            setupLine();
+
+        line.SetVertexCount(points.Count);
+        line.SetPositions(points.ToArray());
+        points.Clear();
+    }
 	#endregion
 
 	#region UpdateRotation
