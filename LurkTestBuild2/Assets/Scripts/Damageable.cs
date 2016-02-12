f4221265f96d84d19f5781169f131b07e357ef04
tmorga18 <EAK@Thomas-Mac.byod.gmu.edu>
2016-02-11 20:24:57 -0500

    bleh

---
+++ LurkTestBuild2/Assets/Scripts/Damageable.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Damageable : MonoBehaviour {
+
+	public int health;
+	public string damagingTag = "Damaging";
+	public bool triggersDealDamage = true;
+	public DeathBehaviour deathBehaviour = DeathBehaviour.DoNothing;
+
+	public DamageTakenEvent onTakeDamage;
+	public UnityEvent onDeath;
+
+	public void TakeDamage(int damage = 1) {
+		TakeDamage(damage, Vector2.zero);
+	}
+
+	public void TakeDamage(int damage, Vector2 direction) {
+		health -= damage;
+		onTakeDamage.Invoke(damage, direction);
+		SendMessage("OnTakeDamage", new Damage(damage, direction));
+		if (health <= 0) {
+			health = 0;
+			Die();
+		}
+	}
+
+	public void TakeDamageFrom(GameObject other) {
+		if (other.tag == damagingTag) {
+			Vector2 direction = transform.position - other.transform.position;
+            Damager damager = other.GetComponent<Damager>();
+			TakeDamage(damager == null ? 1 : damager.damage, direction);
+			if (damager != null) {
+				damager.onDealDamage.Invoke(this, direction);
+				damager.SendMessage("OnDealDamage", this);
+			}
+		}
+	}
+
+	public void Die() {
+		onDeath.Invoke();
+		SendMessage("OnDeath");
+		switch (deathBehaviour) {
+			case DeathBehaviour.Destroy:
+				Destroy(gameObject);
+				break;
+			case DeathBehaviour.Deactivate:
+				gameObject.SetActive(false);
+				break;
+		}
+	}
+
+	void OnCollisonEnter2D(Collision2D collision) {
+		TakeDamageFrom(collision.gameObject);
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (triggersDealDamage) {
+			TakeDamageFrom(collider.gameObject);
+		}
+	}
+
+	public enum DeathBehaviour {
+		DoNothing, Deactivate, Destroy
+	}
+
+}
+
+[System.Serializable]
+public class DamageTakenEvent : UnityEvent<int, Vector2> { }
+
+public struct Damage {
+	float damage;
+	Vector2 direction;
+
+	public Damage(float damage, Vector2 direction) {
+		this.damage = damage;
+		this.direction = direction;
+	}
+}
---
+++ LurkTestBuild2/Assets/Scripts/NPCs/FollowPath.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class FollowPath : MonoBehaviour {
+
+	public Path path;
+	public float speed = 5;
+	public EndBehaviour endBehaviour = EndBehaviour.DoNothing;
+
+	float t = 0;
+
+	void Update() {
+		float length = path.length;
+		float dt = (speed * Time.deltaTime) / path.length;
+		t += dt;
+		switch (endBehaviour) {
+			case EndBehaviour.Loop:
+				t = Mathf.Repeat(t, 1);
+				break;
+			case EndBehaviour.Reverse:
+				if (t > 1 || t < 0) {
+					speed = -speed;
+				}
+				break;
+		}
+		transform.position = path.positionAt(t);
+	}
+
+	public enum EndBehaviour {
+		DoNothing, Loop, Reverse,
+	}
+
+}
