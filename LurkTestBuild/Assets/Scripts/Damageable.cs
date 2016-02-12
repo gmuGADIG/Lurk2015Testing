181686f7a1ecec8e86c2488b414417cd2fc73373
GuitarBro <andrew.arv@gmail.com>
2016-02-11 20:54:18 -0500

    Added working things
    yes

--- LurkTestBuild/Assets/Scripts/Damageable.cs
+++ LurkTestBuild/Assets/Scripts/Damageable.cs
@@ -8,42 +8,19 @@ public class Damageable : MonoBehaviour {
 	public bool triggersDealDamage = true;
 	public DeathBehaviour deathBehaviour = DeathBehaviour.DoNothing;
 
-	public DamageTakenEvent onTakeDamage;
+	public UnityEvent onTakeDamage;
 	public UnityEvent onDeath;
 
-	public void TakeDamage(int damage = 1) {
-		TakeDamage(damage, Vector2.zero);
-	}
-
-	public void TakeDamage(int damage, Vector2 direction) {
-		TakeDamage(null, damage, direction);
-	}
-
-	void TakeDamage(GameObject source, int damage, Vector2 direction) {
-		health -= damage;
-		onTakeDamage.Invoke(damage, direction);
-		SendMessage("OnTakeDamage", new Damage(source, damage, direction));
+	public void TakeDamage() {
+		health --;
+		onTakeDamage.Invoke();
 		if (health <= 0) {
-			health = 0;
 			Die();
-		}
-	}
-
-	public void TakeDamageFrom(GameObject other) {
-		if (other.tag == damagingTag) {
-			Vector2 direction = transform.position - other.transform.position;
-            Damager damager = other.GetComponent<Damager>();
-			TakeDamage(other, damager == null ? 1 : damager.damage, direction);
-			if (damager != null) {
-				damager.onDealDamage.Invoke(this, direction);
-				damager.SendMessage("OnDealDamage", this);
-			}
 		}
 	}
 
 	public void Die() {
 		onDeath.Invoke();
-		SendMessage("OnDeath");
 		switch (deathBehaviour) {
 			case DeathBehaviour.Destroy:
 				Destroy(gameObject);
@@ -54,13 +31,15 @@ public class Damageable : MonoBehaviour {
 		}
 	}
 
-	void OnCollisonEnter2D(Collision2D collision) {
-		TakeDamageFrom(collision.gameObject);
+	void OnCollisonEnter(Collider other) {
+		if (other.tag == damagingTag) {
+			TakeDamage();
+		}
 	}
 
-	void OnTriggerEnter2D(Collider2D collider) {
-		if (triggersDealDamage) {
-			TakeDamageFrom(collider.gameObject);
+	void OnTriggerEnter(Collider other) {
+		if (triggersDealDamage && other.tag == damagingTag) {
+			TakeDamage();
 		}
 	}
 
@@ -68,19 +47,4 @@ public class Damageable : MonoBehaviour {
 		DoNothing, Deactivate, Destroy
 	}
 
-}
-
-[System.Serializable]
-public class DamageTakenEvent : UnityEvent<int, Vector2> { }
-
-public struct Damage {
-	public GameObject source;
-	public float damage;
-	public Vector2 direction;
-
-	public Damage(GameObject source, float damage, Vector2 direction) {
-		this.source = source;
-		this.damage = damage;
-		this.direction = direction;
-	}
 }
--- LurkTestBuild/Assets/Scripts/Inventory.cs
+++ LurkTestBuild/Assets/Scripts/Inventory.cs
@@ -2,9 +2,14 @@
 using System.Collections;
 using UnityEngine.UI;
 
+/// <summary>
+/// Requires Item.cs, Throw.cs, and playerMove.cs
+/// Frequently referenced by playerMove.cs and Throw.cs
+/// </summary>
+
 public class Inventory : MonoBehaviour
 {
-	public Item[] inv = new Item[2];
+	public GameObject[] inv = new GameObject[2];
 	public GameObject[] panels = new GameObject[2];
 	public Sprite defaultImage;
 
@@ -19,37 +24,73 @@ public class Inventory : MonoBehaviour
 	// Switches slot 0 and 1
 	public void SwitchSlots()
 	{
-		Item temp = inv [0];
+		GameObject temp = inv [0];
 		inv [0] = inv [1];
 		inv [1] = temp;
 		UpdateSprites ();
 	}
-
+	
 	// Returns false if inventory slot 0 is full, true otherwise
-	public bool Pickup(Item item)
+	public bool Pickup(GameObject item)
 	{
 		if (inv [0] == null)
 		{
 			inv [0] = item;
 			UpdateSprites();
+			return true;
 		}
-		else
-			return false;
+		
+		return false;
+	}
 
-		return true;
+	// Returns null if inventory slot 0 is empty, the gameObject otherwise
+	public GameObject Drop()
+	{
+		if (inv [0] != null)
+		{
+			// TP to player (with a small offset to avoid ground clipping)
+			inv [0].transform.position = transform.position + new Vector3(0, 0.1f, 0);
+			GameObject tempObject = inv [0];
+			RemoveItem();
+			UpdateSprites();
+			return tempObject;
+		}
+		
+		return null;
+	}
+
+	public void Swap(){
+		GameObject[] tempInv = (GameObject[])inv.Clone();
+		inv [0] = tempInv [1];
+		inv [1] = tempInv [0];
+		UpdateSprites();
+	}
+
+	// Removes item0 and returns it
+	public GameObject RemoveItem()
+	{
+		GameObject temp = inv [0];
+		inv [0] = null;
+		UpdateSprites ();
+		return temp;
+	}
+
+	public GameObject GetItem(int i = 0)
+	{
+		return inv [i];
 	}
 
 	// Should be called every time an item is changed, so it does not need to be called each frame
 	public void UpdateSprites()
 	{
 		// Get object sprites from inv and set GUI images to them
-		if(inv[0] != null)
-			panels [0].GetComponent<Image> ().sprite = inv[0].sprite;
+		if (inv [0] != null)
+			panels [0].GetComponent<Image>().sprite = inv[0].GetComponent<SpriteRenderer> ().sprite;
 		else
 			panels[0].GetComponent<Image>().sprite = defaultImage;
 
 		if(inv[1] != null)
-			panels [1].GetComponent<Image> ().sprite = inv[1].sprite;
+			panels [1].GetComponent<Image>().sprite = inv[1].GetComponent<SpriteRenderer> ().sprite;
 		else
 			panels[1].GetComponent<Image>().sprite = defaultImage;
 	}
