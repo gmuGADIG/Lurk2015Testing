18cf29056acc6b2573b2ad63e1a511d4ad64f48c
Tanner Grehawick <ggrehawi@gmu.edu>
2016-03-03 19:01:05 -0500

    added asset thing for character spritesheets

---
+++ LurkTestBuild/Assets/Scripts/CharacterSpritesheet.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class CharacterSpritesheet : ScriptableObject {
+
+	//placeholders
+	public Texture2D happy;
+	public Texture2D sad;
+	public Texture2D angry;
+
+}
