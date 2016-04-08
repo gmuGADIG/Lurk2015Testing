3b9c57afd881e6822f5590a4b3af589e7925020a
GuitarBro <andrew.arv@gmail.com>
2016-04-07 21:00:52 -0400

    Added lantern
    Added lantern sprite
    Added lantern flame/light

--- LurkTestBuild/Assets/Scripts/Lantern.cs
+++ LurkTestBuild/Assets/Scripts/Lantern.cs
@@ -6,11 +6,13 @@ public class Lantern : Item {
     private Transform player;
     private bool held;
     public Vector3 lanternPos = new Vector3(0, 1, 0);
+	Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start ()
     {
         base.Start();
+		rb = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
